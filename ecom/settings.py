e3e0fb4f822d7098d3131901155ee8ac16cd9c60
Martin Lorenz Fajardo <martinlorenz.fajardo@gmail.com>
2025-09-02 08:18:20 +0800

    latest updates

--- ecom/settings.py
+++ ecom/settings.py
@@ -69,12 +69,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        #'ENGINE': 'django.db.backends.postgresql',
-        #'NAME':,
-        #'USER':,
-        #'PASSWORD':,
-        #'HOST':,
-        #'PORT':,
     }
 }
 
